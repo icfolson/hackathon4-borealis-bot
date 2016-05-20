e8f7701b0cb58d6d003f7230d295d15c59022110
JToussaint <joseph.toussaint@icif.com>
2016-05-20 08:39:28 -0500

    trying to integrate with the new luis app

--- server.js
+++ server.js
@@ -8,13 +8,13 @@ var placeOrderInit = require('./dialogs/place-order');
 var inquireCheeseInit = require('./dialogs/place-order');
 
 //load these values from the environment
-var luisModel = 'https://api.projectoxford.ai/luis/v1/application?id=5cdc03d1-9d72-427b-b763-2054d3877b90&subscription-key=568c2ba8438a4aa3956a2cadf0aad4c1';
+var luisModel = 'https://luis-actions.cloudapp.net/api/v1/botframework?app-id=19d42dd0-96cd-47db-b2a2-259b679e5f45&subscription-key=f7e7b16408d64750ba8188fd25837887';
 var bot = new builder.BotConnectorBot({ appId: process.env.BOT_APP_ID, appSecret: process.env.BOT_APP_SECRET });
 
 //root intent handler
 bot.add('/', new builder.LuisDialog(luisModel)
-    .on('jarvis.order', '/Place-Order')
-    .on('jarvis.cheeses', '/Inquire-Cheese')
+    .on('borealis.maintaining', '/Maintaining')
+    .on('borealis.getting', '/Getting')
     .onDefault(builder.DialogAction.send("I'm sorry I didn't understand."))
 );
 
