0c5192155ff3692e351e02ae4647c1a51df6d79b
critesjm <critesjm@gmail.com>
2016-05-20 14:36:04 -0400

    Example of saving to table storage in getting.js

--- dialogs/getting.js
+++ dialogs/getting.js
@@ -1,10 +1,22 @@
 'use strict';
 
+// Table storage!!
+
+var uuid = require('node-uuid');
+var tableName = "intakes"; // 'TABLE_NAME'
+var partitionKey = "myintakes"; // 'PARTITION_KEY'
+var accountName = "borealiststest"; // 'STORAGE_NAME'
+var accountKey = "kqVzaboKITxfcNdgq9Ws2zX/DGsemwBsXIfGO8CpAmuTZEBnX/ozO2zT1hE1gSi5k2PuRYeDfFnQxuawxvDM7Q=="; // 'STORAGE_KEY'
+var azure = require('azure-storage');
+var async = require('async');
+
 var builder = require('botbuilder');
+
+var Intake = require('../models/intake');
+var intake = new Intake(azure.createTableService(accountName, accountKey), tableName, partitionKey);
 
 const LUIS_ENTITY_ACTIVITY = 'borealis.activity';
 const LUIS_ENTITY_SOURCE = 'borealis.source';
-
 
 const _handleAlcoholSource = (session) => {
     session.endDialog('Alcohol is a common factor in erectile dysfunction.  How often does this occur?');
@@ -25,19 +37,31 @@ const initializeOrder = (session, args, next) => {
     let source = builder.EntityRecognizer.findEntity(args.entities, LUIS_ENTITY_SOURCE);
 
     let luisEntities = session.dialogData.luisEntities;
-
+    
+    var newIntake = { 
+        personId: session.message.from.id,
+        conversationId: session.message.conversationId,
+    };
+    
     if (activity) {
         luisEntities.activity = activity.entity;
+        newIntake.activity = activity.entity;
     }
 
     if (source) {
         luisEntities.source = source.entity;
+        newIntake.source = source.entity;
     }
+    
+    intake.addItem(newIntake, function intakeAdded (error) {
+        if(error) {
+            throw error;
+        }
+    });
 
     _handleAlcoholSource(session);
 };
 /* END: Intake */
-
 
 module.exports = [
     initializeOrder
---
+++ models/intake.js
@@ -0,0 +1,67 @@
+var azure = require('azure-storage');
+var uuid = require('node-uuid');
+var entityGen = azure.TableUtilities.entityGenerator;
+
+module.exports = Intake;
+
+function Intake(storageClient, tableName, partitionKey) {
+  this.storageClient = storageClient;
+  this.tableName = tableName;
+  this.partitionKey = partitionKey;
+  this.storageClient.createTableIfNotExists(tableName, function tableCreated(error) {
+    if(error) {
+      throw error;
+    }
+  });
+};
+
+Intake.prototype = {
+  find: function(query, callback) {
+    self = this;
+    self.storageClient.queryEntities(this.tableName, query, null, function entitiesQueried(error, result) {
+      if(error) {
+        callback(error);
+      } else {
+        callback(null, result.entries);
+      }
+    });
+  },
+
+  addItem: function(item, callback) {
+    self = this;
+    // use entityGenerator to set types
+    // NOTE: RowKey must be a string type, even though
+    // it contains a GUID in this example.
+    var itemDescriptor = {
+      PartitionKey: entityGen.String(self.partitionKey),
+      RowKey: entityGen.String(uuid()),
+      personId: entityGen.String(item.personId),
+      conversationId: entityGen.String(item.conversationId),
+      completed: entityGen.Boolean(false),
+      activity: entityGen.String(item.activity ? typeof(item.activity != 'undefined') : null),
+      source: entityGen.String(item.source ? typeof(item.source) != 'undefined' : null),
+    };
+    self.storageClient.insertEntity(self.tableName, itemDescriptor, function entityInserted(error) {
+      if(error){  
+        callback(error);
+      }
+      callback(null);
+    });
+  },
+
+  updateItem: function(rKey, callback) {
+    self = this;
+    self.storageClient.retrieveEntity(self.tableName, self.partitionKey, rKey, function entityQueried(error, entity) {
+      if(error) {
+        callback(error);
+      }
+      entity.completed._ = true;
+      self.storageClient.updateEntity(self.tableName, entity, function entityUpdated(error) {
+        if(error) {
+          callback(error);
+        }
+        callback(null);
+      });
+    });
+  }
+}
