477d73a4b8dd6aa51868cf49914324535b7414cd
JToussaint <joseph.toussaint@icif.com>
2016-05-20 13:49:34 -0500

    build a generic function for parsing entities

--- dialogs/getting.js
+++ dialogs/getting.js
@@ -10,6 +10,12 @@ const _handleAlcoholSource = (session) => {
     session.endDialog('Alcohol is a common factor in erectile dysfunction.  How often does this occur?');
 };
 
+const _buildLuisEntities = (entities, entityName) => {
+    let parsedEntity = builder.EntityRecognizer.findEntity(entities, entityName);
+    
+    return parsedEntity ? parsedEntity.entity : null;
+};
+
 
 /* BEGIN: Intake */
 const initializeOrder = (session, args, next) => {
@@ -21,18 +27,10 @@ const initializeOrder = (session, args, next) => {
         session.dialogData.luisEntities = {};
     }
 
-    let activity = builder.EntityRecognizer.findEntity(args.entities, LUIS_ENTITY_ACTIVITY);
-    let source = builder.EntityRecognizer.findEntity(args.entities, LUIS_ENTITY_SOURCE);
-
     let luisEntities = session.dialogData.luisEntities;
 
-    if (activity) {
-        luisEntities.activity = activity.entity;
-    }
-
-    if (source) {
-        luisEntities.source = source.entity;
-    }
+    luisEntities.activity = _buildLuisEntities(args.entities, LUIS_ENTITY_ACTIVITY);
+    luisEntities.source = _buildLuisEntities(args.entities, LUIS_ENTITY_SOURCE);
 
     _handleAlcoholSource(session);
 };
