e388236fd0ef945097e2d9fbaa85a18e4c4388ed
JToussaint <joseph.toussaint@icif.com>
2016-05-20 09:47:34 -0500

    updating the bot

--- server.js
+++ server.js
@@ -3,30 +3,36 @@
 var restify = require('restify');
 var builder = require('botbuilder');
 
-//import intents
-var placeOrderInit = require('./dialogs/place-order');
-var inquireCheeseInit = require('./dialogs/place-order');
+//import dialogs
+//var placeOrderInit = require('./dialogs/place-order');
+//var inquireCheeseInit = require('./dialogs/place-order');
 
 //load these values from the environment
 var luisModel = 'https://luis-actions.cloudapp.net/api/v1/botframework?app-id=19d42dd0-96cd-47db-b2a2-259b679e5f45&subscription-key=f7e7b16408d64750ba8188fd25837887';
 var bot = new builder.BotConnectorBot({ appId: process.env.BOT_APP_ID, appSecret: process.env.BOT_APP_SECRET });
 
 //root intent handler
+
+bot.add('/', new builder.LuisDialog(luisModel)
+    .onDefault(builder.DialogAction.send("I'm sorry I didn't understand."))
+);
+
+/*
 bot.add('/', new builder.LuisDialog(luisModel)
     .on('borealis.maintaining', '/Maintaining')
-    .on('borealis.getting', '/Getting')
     .onDefault(builder.DialogAction.send("I'm sorry I didn't understand."))
 );
 
 //place order intent
-bot.add('/Place-Order', 
-    placeOrderInit
+bot.add('/Maintaining', 
+    () => { console.log('hi');}
 );
-
+*/
 //inquire about cheese
-bot.add('/Inquire-Cheese', 
+/*
+bot.add('/Getting', 
     inquireCheeseInit
-);
+);*/
 
 // Setup Restify Server
 var server = restify.createServer();
