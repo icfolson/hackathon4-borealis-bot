956590dad292551ee38c70037cd4b221f05c13b5
JToussaint <joseph.toussaint@icif.com>
2016-05-21 15:41:32 -0500

    moved to a text bot

--- server.js
+++ server.js
@@ -8,6 +8,7 @@ var gettingDialog = require('./dialogs/getting');
 var frequencyDialog = require('./dialogs/frequency');
 
 //load these values from the environment
+/*
 var luisModel = 'https://api.projectoxford.ai/luis/v1/application?id=19d42dd0-96cd-47db-b2a2-259b679e5f45&subscription-key=f7e7b16408d64750ba8188fd25837887';
 var bot = new builder.BotConnectorBot({ appId: process.env.BOT_APP_ID, appSecret: process.env.BOT_APP_SECRET });
 
@@ -28,13 +29,30 @@ bot.add('/', new builder.LuisDialog(luisModel)
 
 bot.add('/Getting',
     gettingDialog
-);
+);*/
+
+var bot = new builder.BotConnectorBot({ appId: process.env.BOT_APP_ID, appSecret: process.env.BOT_APP_SECRET });
+
+var dialog = new builder.CommandDialog();
+
+bot.add('/', dialog);
+
+dialog.matches('.*hard$', [
+    (session) => {
+    builder.Prompts.text(session, 'Drinking can be a cause.  How often does his happen?');
+},
+(session, result) => {
+    session.send('Let’s chat more about this during your visit.')
+}]);
 
 // Setup Restify Server
 var server = restify.createServer();
 
 // Setup the path for the botframework
-server.post('/api/messages', bot.verifyBotFramework(), bot.listen());
+//server.post('/api/messages', bot.verifyBotFramework(), bot.listen());
+//server.post('/api/messages', bot.verifyBotFramework(), bot.listen());
+
+server.post('/api/messages', bot.listen());
 
 // Serve a static web page
 server.get(/.*/, restify.serveStatic({
